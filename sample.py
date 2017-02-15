846cb0e3b43716abdc49c52a7955b7895e08817e
Griffin Bishop <grbishop@wpi.edu>
2017-02-15 13:27:18 -0500

    arguments

--- sample.py
+++ sample.py
@@ -1,2 +1,13 @@
 #File: sample.py
 #Authors: Griffin Bishop, David Deisde, Gianluca Tarquinio, Godot
+
+import sys, random, math
+
+
+
+def main():
+    arguments = sys.argv
+    
+
+if __name__ == '__main__':
+    main()
