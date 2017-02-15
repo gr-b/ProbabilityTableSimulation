d28ec3fe7e2713be1123a54469b1ec2cc27d99f6
ddeisadze <tbilisidavid@hotmail.com>
2017-02-15 13:25:07 -0500

    Added node class

--- sample.py
+++ sample.py
@@ -1,2 +1,9 @@
 #File: sample.py
 #Authors: Griffin Bishop, David Deisde, Gianluca Tarquinio, Godot
+
+class Node(object):
+    """
+    Node object, has reference to its parent
+    """
+    def __init__(self, parent=None):
+        self.parent = None
