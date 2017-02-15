e501f97a3ae98dbd1822a8cea5b2c33a2b197e3b
ddeisadze <tbilisidavid@hotmail.com>
2017-02-15 15:03:51 -0500

    Added Base node get probability function

--- sample.py
+++ sample.py
@@ -1,5 +1,7 @@
 #File: sample.py
 #Authors: Griffin Bishop, David Deisadze, Gianluca Tarquinio, Godot
+import random
+
 
 class Node(object):
     """
@@ -8,3 +10,23 @@ class Node(object):
     def __init__(self, parent=None):
         self.parent = parent
 
+# gets random number [0, 1]
+def rollDice():
+    return random.random()
+
+# uses random probability to get the key for genrerated probability
+# if not in range, returns None
+# returns tuple (key, val)
+def baseNodeGetProbability(base_node):
+    die_rolled = rollDice()
+    print die_rolled
+
+    # get all indices after the first
+    for key in sorted(base_node, key=base_node.get):
+        if key != 'dependent_on':
+            val = base_node[key]
+
+            if die_rolled - val < 0:
+                return (key, base_node[key])
+
+# print baseNodeGetProbability(humidity)
